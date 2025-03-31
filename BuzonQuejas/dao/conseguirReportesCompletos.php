75ffcaaf57e24042db2a5128498a21ee2cc72601
Iván Medina <mivan0851@gmail.com>
2025-03-31 11:56:47 -0600

    Arreglando supervisor en tabla 2

--- BuzonQuejas/dao/conseguirReportesCompletos.php
+++ BuzonQuejas/dao/conseguirReportesCompletos.php
@@ -9,17 +9,22 @@ try {
     // Solo reportes con FechaFinalizada válida
     $query = $conn->prepare("
         SELECT 
-            FolioReportes AS folio,
-            NumeroNomina AS nomina,
-            IFNULL(IdEncargado, 'N/A') AS encargado,
-            FechaRegistro AS fechaRegistro,
-            FechaFinalizada AS fechaFinalizacion,
-            Descripcion AS descripcion,
-            IFNULL(Comentarios, 'Sin comentarios') AS comentarios,
-            'Completado' AS estatus
-        FROM Reporte
-        WHERE FechaFinalizada IS NOT NULL AND FechaFinalizada <> '0000-00-00 00:00:00'
-        ORDER BY FechaFinalizada DESC
+          r.FolioReportes AS folio,
+    r.NumeroNomina AS nomina,
+    CONCAT(
+        '<strong>SUPERVISOR:</strong> <span class=\"nombre-encargado\">', IFNULL(sup.NombreEncargado, 'N/A'), '</span><br>',
+        '<strong>SHIFT LEADER:</strong> <span class=\"nombre-encargado\">', IFNULL(shift.NombreEncargado, 'N/A'), '</span>'
+    ) AS encargado,
+    r.FechaRegistro AS fechaRegistro,
+    r.FechaFinalizada AS fechaFinalizacion,
+    r.Descripcion AS descripcion,
+    IFNULL(r.Comentarios, 'Sin comentarios') AS comentarios,
+    'Completado' AS estatus
+FROM Reporte r
+LEFT JOIN Encargado sup ON r.IdEncargado = sup.IdEncargado
+LEFT JOIN Encargado shift ON r.IdShiftLeader = shift.IdEncargado
+WHERE r.FechaFinalizada IS NOT NULL AND r.FechaFinalizada <> '0000-00-00 00:00:00'
+ORDER BY r.FechaFinalizada DESC
     ");
 
     $query->execute();
