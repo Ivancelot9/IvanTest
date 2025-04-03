8ca28e58366b57edc56af75297e28cd1ade4b0e2
Iván Medina <mivan0851@gmail.com>
2025-04-03 09:16:53 -0600

    Agregandfo Foto del Usuario

--- BuzonQuejas/dashboardAdmin.php
+++ BuzonQuejas/dashboardAdmin.php
@@ -49,7 +49,7 @@ if ($_SESSION["NumNomina"] == "" && $_SESSION["NumNomina"]== null && $_SESSION["
     <img src="imagenes/superhero-sinfondo.png" class="hero-animation">
 
     <div class="profile">
-        <img src="imagenes/user.png" alt="Profile Picture">
+        <img src="https://grammermx.com/Fotos/<?php echo $_SESSION["NumNomina"]?>.png" alt="Profile Picture">
         <div class="username-bubble">
             <h4 id="sidebar-nombre"></h4>
         </div>
