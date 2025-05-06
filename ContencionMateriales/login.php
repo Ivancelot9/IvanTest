9905e79b8085b4b1bb488a0528aface59dede1c9
Iván Medina <mivan0851@gmail.com>
2025-05-06 11:05:33 -0600

    Login Contencion

--- ContencionMateriales/login.php
+++ ContencionMateriales/login.php
@@ -13,6 +13,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/loginAdminContenciones.css">
+
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
