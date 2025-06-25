53683db9247c5b8d948c80e10336607f5cbebf24
Iván Medina <mivan0851@gmail.com>
2025-06-25 14:39:34 -0600

    Envio a Externos

--- ContencionMateriales/js/modalEnviarCorreos.js
+++ ContencionMateriales/js/modalEnviarCorreos.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 formData.append('correo', email);
                 folios.forEach(folio => formData.append('folios[]', folio));
 
-                const resp = await fetch('Mailer/enviarCorreoaExterno.php', {
+                const resp = await fetch('https://grammermx.com/Mailer/enviarCorreoaExterno.php', {
                     method: 'POST',
                     body: formData
                 });
